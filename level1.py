9302e9828d643dd692308ed2ae1d925663d4223b
Jolineblt <jolineboldt@gmail.com>
2026-02-20 09:33:46 +0100

    a

--- level1.py
+++ level1.py
@@ -1,35 +1,39 @@
 from kivy.app import App  
 from kivy.uix.gridlayout import GridLayout  
 from kivy.uix.button import Button  
-#from kivy.uix.label import Label  
+from kivy.uix.label import Label  
 
-class Level1App(App):  
-    def build(self):    
-
-        layout = GridLayout(cols=4, row=4)   
-        def __init__(self, **kwargs):         
-            super().__init__(**kwargs)    
-            grid.add_widget(Button(text="Memory.io", font_size=20))  
-            grid.add_widget(Button(text="Memory.io", font_size=20))  
-            grid.add_widget(Button(text="Memory.io", font_size=20))  
-            grid.add_widget(Button(text="Memory.io", font_size=20))
-            grid.add_widget(Button(text="Memory.io", font_size=20))  
-            grid.add_widget(Button(text="Memory.io", font_size=20))  
-            grid.add_widget(Button(text="Memory.io", font_size=20))  
-            grid.add_widget(Button(text="Memory.io", font_size=20))
-            grid.add_widget(Button(text="Memory.io", font_size=20))  
-            grid.add_widget(Button(text="Memory.io", font_size=20))  
-            grid.add_widget(Button(text="Memory.io", font_size=20))  
-            grid.add_widget(Button(text="Memory.io", font_size=20))
-            grid.add_widget(Button(text="Memory.io", font_size=20))  
-            grid.add_widget(Button(text="Memory.io", font_size=20))  
-            grid.add_widget(Button(text="Memory.io", font_size=20))  
-            grid.add_widget(Button(text="Memory.io", font_size=20))
-        return layout
+class MyGridApp(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # Set grid size
+        self.cols = 6
+        self.rows = 6
+        grid.add_widget(Button(text="Memory.io", font_size=20))  
+        grid.add_widget(Button(text="Memory.io", font_size=20))  
+        grid.add_widget(Button(text="Memory.io", font_size=20))  
+        grid.add_widget(Button(text="Memory.io", font_size=20))
+        grid.add_widget(Button(text="Memory.io", font_size=20))  
+        grid.add_widget(Button(text="Memory.io", font_size=20))  
+        grid.add_widget(Button(text="Memory.io", font_size=20))  
+        grid.add_widget(Button(text="Memory.io", font_size=20))
+        grid.add_widget(Button(text="Memory.io", font_size=20))  
+        grid.add_widget(Button(text="Memory.io", font_size=20))  
+        grid.add_widget(Button(text="Memory.io", font_size=20))  
+        grid.add_widget(Button(text="Memory.io", font_size=20))
+        grid.add_widget(Button(text="Memory.io", font_size=20))  
+        grid.add_widget(Button(text="Memory.io", font_size=20))  
+        grid.add_widget(Button(text="Memory.io", font_size=20))  
+        grid.add_widget(Button(text="Memory.io", font_size=20))
     
 
     def button_gedrueckt(self, instance):
         print(instance.text + "wurde gedrückt")
         instance.background_color = (0, 0, 1, 1)
 
-Level1App().run()
+
+class GridApp(App):
+    def build(self):
+        return MyGrid()
+
+MyGridApp().run()
