6c2d899a0f29cc6d461fb0f8c4d23cb5898ee23f
lisa631 <hxhsd1237@gmail.com>
2026-02-23 11:14:30 +0100

    buttons levels

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from kivy.app import App  
-from kivy.uix.gridlayout import GridLayout  
 from kivy.uix.button import Button  
-from kivy.uix.label import Label  
+from kivy.uix.boxlayout import BoxLayout
+#from kivy.uix.label import Label  
+
+box = BoxLayout(orientation='vertical')  
+box.add_widget(Button(text='Level 1'))  
+box.add_widget(Button(text='Level 2'))  
+box.add_widget(Button(text='Level 3'))
 
 
 MainApp().run()
