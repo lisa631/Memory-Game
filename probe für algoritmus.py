b086c33823dff44516d6db168cc780f1488db8f6
CvH-07 <cvh@ahig-group.com>
2026-02-27 11:24:43 +0100

    erster Entwurf des algorthmuses
    Refactor image loading and button handling in the Kivy app.

--- probe für algoritmus.py
+++ probe für algoritmus.py
@@ -2,52 +2,49 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout  
 from kivy.uix.button import Button  
 from kivy.uix.label import Label  
+from kivy.clock import Clock
 import random
 from PIL import Image
 
-a = Image.open("bear.jpeg")
-b = Image.open("cat.jpeg")
-c = Image.open("cow.jpeg")
-d = Image.open("dog.jpeg")
-e = Image.open("elefant.jpeg")
-f = Image.open("fox.jpeg")
-g = Image.open("koala.jpeg")
-h = Image.open("lama.jpeg")
-i = Image.open("bear2.jpeg")
-j = Image.open("cat2.jpeg")
-k = Image.open("cow2.jpeg")
-l = Image.open("dog2.jpeg")
-m = Image.open("elefant2.jpeg")
-n = Image.open("fox2.jpeg")
-o = Image.open("koala2.jpeg")
-p = Image.open("lama2.jpeg")
+
+bilder = ["bear.jpeg", "cat.jpeg", "cow.jpeg", "dog.jpeg", "elefant.jpeg", "fox.jpeg", "koala.jpeg", "lama.jpeg", "bear2.jpeg", "cat2.jpeg", "cow2.jpeg", "dog2.jpeg", "elefant2.jpeg", "fox2.jpeg", "koala2.jpeg", "lama2.jpeg"]
+
+global counter = 0
+global aufgedeckte_buttons = []
 
 
-bilder = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p]
-
-button_choice = None
 class GridApp(App):
     def build(self):
-       # Set grid size
-        self.cols = 4
+        grid = GridLayout(cols= 4, padding=20, spacing=10)
         self.rows = 4
         random.shuffle(bilder)
-        grid = GridLayout(cols= self.cols, padding=20, spacing=10)
-
-        for i in range(1, 17):
-            button = Button(background_color=(0, 0, 1, 1))
-            grid.add_widget(button)
-
         
-        def button_pressed(choice):
-            global button_choice
-            button_choice = choice
-            if button_choice == button:
-                button.background_color = (0, 0, 0, 0)
+
+        for bild in bilder:
+            button = Button(
+                background_color=(0, 0, 1, 1),  
+                
+            button.bild = bild 
+            button.bind(on_press=self.button_pressed)
+            grid.add_widget(button)
                 
 
         return grid
     
+def button_pressed(self, butten):
+    
+    button.background_color = (0, 0, 1, 0)
+    aufgedeckte_buttons.append(button)
+    counter += 1
+
+    if coounter == 2:
+        Clock.schedule_once(self.zuruecksetzen, 5)
+
+def zuruecksetzen(self, dt):
+    for button in aufgedeckte_buttons:
+        button.background_color = (0, 0, 1, 1)
+    counter = 0
+    aufgedeckte_buttons = []
 
 if __name__ == "__main__":
     GridApp().run()
