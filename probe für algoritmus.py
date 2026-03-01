59b51aa00d27594ac1a1f878baf5649603cf8eb2
lisa631 <hxhsd1237@gmail.com>
2026-03-01 22:40:57 +0100

    update

--- probe für algoritmus.py
+++ probe für algoritmus.py
@@ -4,17 +4,17 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label  
 from kivy.clock import Clock
 import random
-from PIL import Image
+#from PIL import Image
 
 
 bilder = ["bear.jpeg", "cat.jpeg", "cow.jpeg", "dog.jpeg", "elefant.jpeg", "fox.jpeg", "koala.jpeg", "lama.jpeg", "bear2.jpeg", "cat2.jpeg", "cow2.jpeg", "dog2.jpeg", "elefant2.jpeg", "fox2.jpeg", "koala2.jpeg", "lama2.jpeg"]
 
-global counter = 0
-global aufgedeckte_buttons = []
-
 
 class GridApp(App):
     def build(self):
+        self.counter = 0
+        self.aufgedeckte_buttons = []
+
         grid = GridLayout(cols= 4, padding=20, spacing=10)
         self.rows = 4
         random.shuffle(bilder)
@@ -22,7 +22,7 @@ class GridApp(App):
 
         for bild in bilder:
             button = Button(
-                background_color=(0, 0, 1, 1),  
+                background_color=(0, 0, 1, 1))  
                 
             button.bild = bild 
             button.bind(on_press=self.button_pressed)
@@ -31,20 +31,20 @@ class GridApp(App):
 
         return grid
     
-def button_pressed(self, butten):
-    
-    button.background_color = (0, 0, 1, 0)
-    aufgedeckte_buttons.append(button)
-    counter += 1
+    def button_pressed(self, button):
+        
+        button.background_color = (0, 0, 1, 0)
+        self.aufgedeckte_buttons.append(button)
+        self.counter += 1
 
-    if coounter == 2:
-        Clock.schedule_once(self.zuruecksetzen, 5)
+        if self.counter == 2:
+            Clock.schedule_once(self.zuruecksetzen, 5)
 
-def zuruecksetzen(self, dt):
-    for button in aufgedeckte_buttons:
-        button.background_color = (0, 0, 1, 1)
-    counter = 0
-    aufgedeckte_buttons = []
+    def zuruecksetzen(self, dt):
+        for button in self.aufgedeckte_buttons:
+            button.background_color = (0, 0, 1, 1)
+        self.counter = 0
+        self.aufgedeckte_buttons = []
 
 if __name__ == "__main__":
     GridApp().run()
